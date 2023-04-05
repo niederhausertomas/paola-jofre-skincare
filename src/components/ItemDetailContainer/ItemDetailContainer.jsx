dc831befd7b43f0e6f2b7e42a3c09c0538962858
Tomas Niederhauser <niederhausertomas@gmail.com>
2023-04-05 21:35:54 +0200

    consumiendo firebase desde itemDetail

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -3,25 +3,39 @@ import './ItemDetailContainer.css';
 import ItemDetail from '../ItemDetail/ItemDetail';
 import { useParams } from 'react-router-dom';
 import BounceLoader from 'react-spinners/BounceLoader';
+import { collection, doc, getDoc } from 'firebase/firestore';
+import { db } from '../../services/firebaseConfig';
+
 
 
 const ItemDetailContainer = () => {
-    const [items, setItems] = useState([]);
-    const [loading, setLoading] = useState(true);
-    let id = useParams();
-    useEffect(()=>{
-      fetch('https://fakestoreapi.com/products')
-      .then((res) => res.json())
-      .then((res) => setItems(res))
-      .catch((error)=> console.log(error))
-      .finally(()=> setLoading(false))
-    },[]);
-    id = parseInt(id.idNumber)
-    const productDetail = items.find((product)=>product.id===id);
+  const [item, setItem] = useState({});
+  const [loading, setLoading] = useState(true);
+  const { idNumber } = useParams();
+    
+    
+  useEffect(() => {
+    const collectionProd = collection(db, 'productos');
+    const ref = doc(collectionProd, idNumber);
+
+    getDoc(ref)
+        .then((res) => {
+            setItem({
+                id: res.id,
+                ...res.data(),
+            });
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+        .finally(() => {
+            setLoading(false);
+        });
+  }, [idNumber]);
 
     return (
         <div className='container d-flex justify-content-evenly flex-wrap'>
-          { loading ? <BounceLoader className='container' color="hsla(293, 80%, 79%, 1)" /> : <ItemDetail productDetail ={productDetail} />}
+          { loading ? <BounceLoader className='container' color="hsla(293, 80%, 79%, 1)" /> : <ItemDetail productDetail ={item} />}
         </div>
     )
 }
